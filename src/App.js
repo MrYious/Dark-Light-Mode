6270b09e32af6704f080074235d804f06a921bc9
MrYious <rosariomark37@gmail.com>
2022-03-15 21:35:37 +0800

    added distinct theme for dark and light mode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react';
 import { ThemeModeContext } from './context/ThemeModeContext';
 import Switch from './component/Switch';
+import { CssBaseline } from '@mui/material';
 
-function App() {
+export default function App() {
   //State for the current mode [dark or light]
   const [mode, setMode] = useState('light');
 
@@ -17,18 +18,43 @@ function App() {
   //Theme for MUI ThemeContext 
   const theme = createTheme({
     palette: {
-      mode,
-    },
+      mode,      
+      ...( mode === 'light'
+      ? {
+        // Light Mode
+        primary: {main: '#00796b',
+        },
+        secondary: {
+          main: 'rgb(220, 0, 78)',
+        },
+        background: {
+          default: '#fff',
+          paper: '#fff',
+        },
+      }
+      : {
+          // Dark Mode
+          primary: {
+            main: '#00796b',
+          },
+          secondary: {
+            main: '#311b92',
+          },
+          background: {
+            default: '#212121',
+            paper: '#424242',
+          },
+        }
+      )
+    }
   });
 
   return (<>
     <ThemeModeContext.Provider value={themeMode}>    
-      <ThemeProvider theme={theme}>  
-          <Switch/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Switch/>
       </ThemeProvider>
     </ThemeModeContext.Provider>
-  </>);
-  
+  </>);  
 }
-
-export default App;
--- src/component/Switch.js
+++ src/component/Switch.js
@@ -1,15 +1,24 @@
 import { useContext } from "react";
-import { Button } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, Container, Box, TextField } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import { ThemeModeContext } from "../context/ThemeModeContext";
+import SimpleAccordion from "./Accordion";
 
 export default function Switch (){    
   const handleToggleMode = useContext(ThemeModeContext);
   const theme = useTheme();
-  
-  return <Box>    
-    <Button variant="text" onClick={handleToggleMode.toggleThemeMode}>Toggle Color</Button>
-    {theme.palette.mode} mode
-  </Box>
+
+  console.log(theme);
+
+  return <>
+    <Container maxWidth={"sm"}>
+      <Button variant="contained" onClick={handleToggleMode.toggleThemeMode}>Toggle Color</Button>
+      <SimpleAccordion/>
+      <Box >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+      </Box>
+    </Container>    
+  </>
 }
